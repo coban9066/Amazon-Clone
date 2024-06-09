e3e614531c749c4c7e6ba9dbfb8d243a536aa4d0
Mehmet Çoban <137454414+coban9066@users.noreply.github.com>
2024-06-10 02:43:55 +0300

    v3

---
+++ source/script.js
@@ -0,0 +1,33 @@
+function toggleSupportForm() {
+    var supportForm = document.getElementById("supportForm");
+    supportForm.classList.toggle("show");
+}
+
+function submitSupportForm(event) {
+    event.preventDefault(); 
+    var email = document.getElementById("supportFormContent").elements["email"].value;
+    var message = document.getElementById("supportFormContent").elements["message"].value;
+   
+    alert("Destek talebiniz alındı. En kısa sürede size geri dönüş yapılacaktır.");
+}
+document.getElementById("searchInput").addEventListener("keypress", function(event) {
+
+    if (event.key === 'Enter') {
+        searchOnAmazon();
+    }
+});
+
+document.getElementById("searchButton").addEventListener("click", function() {
+    searchOnAmazon();
+});
+
+function searchOnAmazon() {
+
+    var searchTerm = document.getElementById("searchInput").value;
+
+ 
+    var amazonURL = "https://www.amazon.com.tr/s?k=" + encodeURIComponent(searchTerm);
+
+
+    window.open(amazonURL, "_blank");
+}
